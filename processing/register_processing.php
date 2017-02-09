7286bba2c0425f0b14ff1958bc364bb05438e293
Sean Davis <fridurmus@gmail.com>
2017-02-09 01:42:06 -0500

    User Registration
    New users can now be registered

--- processing/register_processing.php
+++ processing/register_processing.php
@@ -1,10 +1,26 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: fridurmus
- * Date: 2/8/17
- * Time: 12:17 AM
+ * User: Sean Davis
+ * Date: 1/18/2017
+ * Time: 12:04 PM
  */
+require_once '../assets/database_functions.php';
+$userName = $_POST['newUserName'];
+$loginName = $_POST['newLoginName'];
+$newPassword = password_hash($_POST['newPassword'], PASSWORD_DEFAULT);
 
-echo "|s";
+$sql = "INSERT INTO users (user_name, login_name, user_pw_hash)
+        VALUES (:user_name, :login_name, :passHash)";
+
+$vars = array(':user_name'=>$userName, ':login_name'=>$loginName, ':passHash'=>$newPassword);
+$insertResult = pdoInsert($sql, $vars);
+
+if($insertResult){
+    echo '|s';
+}
+
+else {
+    echo '|e';
+}
 ?>
